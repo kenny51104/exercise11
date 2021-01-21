a3dd4e9c17bef53493e630e366d44897f6f0bf88
kenny51104 <kenny51104@yahoo.com.tw>
2021-01-21 11:20:01 +0800

    'first_commit'

---
+++ Untitled2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+a = np.arange(0, 21, 1)
+a
+
+
+# In[6]:
+
+
+a[::2]
+
+
+# In[8]:
+
+
+a[::3]
+
+
+# In[ ]:
+
+
+
+
